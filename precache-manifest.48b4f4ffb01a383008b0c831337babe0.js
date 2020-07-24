a5ce2ddd0edc0a42719031eb6ceaffb6f9bd78a1
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-07-24 16:10:54 -0400

    Updates

---
+++ precache-manifest.48b4f4ffb01a383008b0c831337babe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7516dc144fa329a6fca3c4079f7d43be",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "1c515ebfe660c1634529",
+    "url": "/project-4-assessment/static/css/main.183ef10f.chunk.css"
+  },
+  {
+    "revision": "31bfa2d51618cd436f52",
+    "url": "/project-4-assessment/static/js/2.8a2ae7bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.8a2ae7bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c515ebfe660c1634529",
+    "url": "/project-4-assessment/static/js/main.59d357da.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
